be346c69e54a612a29028400ad7eca67dbe4af78
KORuL <korul1@yandex.ru>
2018-08-25 07:34:00 -0700

    * new ex

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
-#![cfg_attr(feature = "nightly", feature(alloc_system))]
+//! Hello world server.
+//!
+//! A simple server that accepts connections, writes "hello world\n", and closes
+//! the connection.
+//!
+//! You can test this out by running:
+//!
+//!     cargo run --example hello_world
+//!
+//! And then in another terminal run:
+//!
+//!     telnet localhost 6142
+//!
 
-#[cfg(feature = "nightly")]
-extern crate alloc_system;
-#[macro_use]
-extern crate log;
+#![deny(warnings)]
 
 extern crate tokio;
 
@@ -11,23 +20,52 @@ use tokio::io;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 
-#[no_mangle]
-pub extern fn hello_world() {
-let addr = "127.0.0.1:6142".parse().unwrap();
+#[macro_use] extern crate log;
+extern crate android_logger;
 
+use android_logger::Filter;
+use log::Level;
+
+fn native_activity_create() {
+    android_logger::init_once(Filter::default().with_min_level(Level::Trace), None);
+}
+
+#[no_mangle]
+pub extern fn hello_world() -> i32 {
+
+    native_activity_create();
+
+    warn!("I am Started");
+    
+    5+5
+}
+
+
+#[no_mangle]
+pub extern fn hello_world1() -> i32 {
+    
+    let addr = "127.0.0.1:6142".parse().unwrap();
+
+    warn!("before listener bind");
     // Bind a TCP listener to the socket address.
     //
     // Note that this is the Tokio TcpListener, which is fully async.
-    let listener = TcpListener::bind(&addr).unwrap();
+    let listener = TcpListener::bind(&addr);
+
+    warn!("after listener bind");
+    
+    let listener_unwrap = listener.unwrap();
+
+    warn!("listener_unwrap");
 
     // The server task asynchronously iterates over and processes each
     // incoming connection.
-    let server = listener.incoming().for_each(|socket| {
-        println!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
+    let server = listener_unwrap.incoming().for_each(|socket| {
+        warn!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
 
         let connection = io::write_all(socket, "hello world\n")
             .then(|res| {
-                println!("wrote message; success={:?}", res.is_ok());
+                warn!("wrote message; success={:?}", res.is_ok());
                 Ok(())
             });
 
@@ -41,10 +79,10 @@ let addr = "127.0.0.1:6142".parse().unwrap();
         // handling and helps avoid silencing failures.
         //
         // In our example, we are only going to log the error to STDOUT.
-        println!("accept error = {:?}", err);
+        warn!("accept error = {:?}", err);
     });
 
-    println!("server running on localhost:6142");
+    warn!("server running on localhost:6142");
 
     // Start the Tokio runtime.
     //
@@ -59,6 +97,10 @@ let addr = "127.0.0.1:6142".parse().unwrap();
     // In our example, we have not defined a shutdown strategy, so this will
     // block until `ctrl-c` is pressed at the terminal.
     tokio::run(server);
+    warn!("tokio run");
+
+
+    5+5
 }
 
 /// Expose the JNI interface for android below
@@ -70,14 +112,19 @@ pub mod android {
     use super::*;
     use self::jni::JNIEnv;
     use self::jni::objects::{JClass};
-    use self::jni::sys::{jboolean};
+    use self::jni::sys::{jint};
 
     #[no_mangle]
-    pub unsafe extern fn Java_ru_hintsolution_hbbft_hbbft_MainActivity_helloworld(_env: JNIEnv, _: JClass) -> jboolean {
+    pub unsafe extern fn Java_ru_hintsolution_hbbft_hbbft_MainActivity_helloworld(_env: JNIEnv, _: JClass) -> jint {
         // Our Java companion code might pass-in "world" as a string, hence the name.
-        hello_world();
-        1
+        let a = hello_world();
+        a
+    }
+
+    #[no_mangle]
+    pub unsafe extern fn Java_ru_hintsolution_hbbft_hbbft_MainActivity_helloworld1(_env: JNIEnv, _: JClass) -> jint {
+        // Our Java companion code might pass-in "world" as a string, hence the name.
+        let a = hello_world1();
+        a
     }
 }
-
-
